1ca903f3e0f229915fbdc243a5b3468d3179542f
AlvinChiou <alvinhsian@gmail.com>
2014-05-08 02:20:20 +0800

    ProdItemService finished

--- src/com/productitem/model/ProdItemService.java
+++ src/com/productitem/model/ProdItemService.java
@@ -1,14 +1,38 @@
 package com.productitem.model;
 import java.util.*;
+import com.sun.org.apache.regexp.internal.recompile;
 public class ProdItemService {
 	private ProdItemDAO_interface dao;
 	public ProdItemService(){
 		dao = new ProdItemDAO();
 	}
 	public ProdItemVO addProdItem(Integer itemno, Integer itemqty, String itemmemo, String ordno, Integer prono){
-		ProdItemVO prodItemVO = new ProdItemVO();
-		
+		ProdItemVO prodItemVO = new ProdItemVO();	
+		prodItemVO.setItemmemo(itemmemo);
+		prodItemVO.setItemqty(itemqty);
+		prodItemVO.setItemno(itemno);
+		prodItemVO.setOrdno(ordno);
+		prodItemVO.setProno(prono);
+		dao.insert(prodItemVO);
 		return prodItemVO;
 	}
-	
+	public ProdItemVO updateProdItemVO(Integer itemno, Integer itemqty, String itemmemo, String ordno, Integer prono){
+		ProdItemVO prodItemVO = new ProdItemVO();
+		prodItemVO.setItemmemo(itemmemo);
+		prodItemVO.setItemqty(itemqty);
+		prodItemVO.setItemno(itemno);
+		prodItemVO.setOrdno(ordno);
+		prodItemVO.setProno(prono);
+		dao.update(prodItemVO);
+		return prodItemVO;
+	}
+	public List<ProdItemVO> getAll(){
+		return dao.getAll();
+	}
+	public ProdItemVO getOnProdItemVO(int itemno){
+		return dao.findByPrimaryKey(itemno);
+	}
+	public void deleteProdItem(int itemno){
+		dao.delete(itemno);
+	}
 }
